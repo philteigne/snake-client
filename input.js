aa903633ecd3f2d36377c1ee2e74c6e41ffea34f
Phil <philippe.teigne@gmail.com>
2023-11-04 18:42:23 -0400

    input now toggles

--- input.js
+++ input.js
@@ -3,6 +3,7 @@ const { INPUT_MAP } = require('./constants');
 
 // Stores the active TCP connection object.
 let connection;
+let moveInterval;
 
 const handleUserInput = function(key) {
   if (key === '\u0003') { //  exits process when user inputs ctrl+C
@@ -13,17 +14,24 @@ const handleUserInput = function(key) {
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
+
   stdin.setRawMode(true); //  removes ability to use ctrl+C to exit
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
   stdin.on("data", interpretInput);
+
   return stdin;
 };
 
 // interpret user input as directional movements in the game
 const interpretInput = (key) => {
-  connection.write(INPUT_MAP[key]);
+  clearInterval(moveInterval);
+  moveInterval = setInterval(() => {
+    connection.write(INPUT_MAP[key]);
+  }, 50);
 };
+
+
 
 module.exports = { setupInput, };
