0728643230cb6eb8078948d4a2179bbf7989b03a
Phil <philippe.teigne@gmail.com>
2023-11-04 16:19:30 -0400

    move handle user input and setupInput to new file input.js. Moved stdin call on handleUserInput to within setupInput.

---
+++ input.js
@@ -0,0 +1,18 @@
+const { stdin } = require('process');
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') { //  exits process when user inputs ctrl+C
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true); //  removes ability to use ctrl+C to exit
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const { stdin } = require('process');
 
 const connect = require('./client').connect;
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true); //  removes ability to use ctrl+C to exit
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') { //  exits process when user inputs ctrl+C
-    process.exit();
-  }
-};
-
-stdin.on("data", handleUserInput);
+const input = require('./input');
 
 console.log("Connecting ...");
 connect();
-setupInput();
+input();
