fa800614540632f007679c8dd7efea72bf6b5eea
Phil <philippe.teigne@gmail.com>
2023-11-04 17:13:47 -0400

    move constant variables to constant file then import relevant properties to input and client

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = '172.25.182.136';
+const PORT = 50541;
+const PLAYER_INITIALS = "PVT";
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const SAY_MSG_01 = 'j';
+const SAY_MSG_02 = 'k';
+const SAY_MSG_03 = 'l';
+
+module.exports = {
+  IP,
+  PORT,
+  PLAYER_INITIALS,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  SAY_MSG_01,
+  SAY_MSG_02,
+  SAY_MSG_03
+};
